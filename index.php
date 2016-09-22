d8b78134128e051bf2a094e4402038add45bdee9
Claudio <jccp74@hotmail.com>
2016-09-22 11:57:47 -0300

    Alteração no nome do Sistema no Index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
-        <title>Sistema Médico de Especialidades Online</title>
+        <title>Sistema Médico de Especialidades Online - Ortopedia</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="shortcut icon" href="app.public/img/favicon.ico">
